0e62a75174567a771a2468ffbd4bab02c9959363
hiyori99 <82105285+hiyori99@users.noreply.github.com>
2021-08-16 00:49:06 +0900

    update files

--- finance/application.py
+++ finance/application.py
@@ -1,3 +1,146 @@
+# import os
+
+# from cs50 import SQL
+# from flask import Flask, flash, redirect, render_template, request, session
+# from flask_session import Session
+# from tempfile import mkdtemp
+# from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+# from werkzeug.security import check_password_hash, generate_password_hash
+
+# from helpers import apology, login_required, lookup, usd
+
+# # Configure application
+# app = Flask(__name__)
+
+# # Ensure templates are auto-reloaded
+# app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# # Ensure responses aren't cached
+# @app.after_request
+# def after_request(response):
+#     response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     response.headers["Expires"] = 0
+#     response.headers["Pragma"] = "no-cache"
+#     return response
+
+
+# # Custom filter
+# app.jinja_env.filters["usd"] = usd
+
+# # Configure session to use filesystem (instead of signed cookies)
+# app.config["SESSION_FILE_DIR"] = mkdtemp()
+# app.config["SESSION_PERMANENT"] = False
+# app.config["SESSION_TYPE"] = "filesystem"
+# Session(app)
+
+# # Configure CS50 Library to use SQLite database
+# db = SQL("sqlite:///finance.db")
+
+# # Make sure API key is set
+# if not os.environ.get("API_KEY"):
+#     raise RuntimeError("API_KEY not set")
+
+
+# @app.route("/")
+# @login_required
+# def index():
+#     """Show portfolio of stocks"""
+#     return apology("TODO")
+
+
+# @app.route("/buy", methods=["GET", "POST"])
+# @login_required
+# def buy():
+#     """Buy shares of stock"""
+#     return apology("TODO")
+
+
+# @app.route("/history")
+# @login_required
+# def history():
+#     """Show history of transactions"""
+#     return apology("TODO")
+
+
+# @app.route("/login", methods=["GET", "POST"])
+# def login():
+#     """Log user in"""
+
+#     # Forget any user_id
+#     session.clear()
+
+#     # User reached route via POST (as by submitting a form via POST)
+#     if request.method == "POST":
+
+#         # Ensure username was submitted
+#         if not request.form.get("username"):
+#             return apology("must provide username", 403)
+
+#         # Ensure password was submitted
+#         elif not request.form.get("password"):
+#             return apology("must provide password", 403)
+
+#         # Query database for username
+#         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+#         # Ensure username exists and password is correct
+#         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+#             return apology("invalid username and/or password", 403)
+
+#         # Remember which user has logged in
+#         session["user_id"] = rows[0]["id"]
+
+#         # Redirect user to home page
+#         return redirect("/")
+
+#     # User reached route via GET (as by clicking a link or via redirect)
+#     else:
+#         return render_template("login.html")
+
+
+# @app.route("/logout")
+# def logout():
+#     """Log user out"""
+
+#     # Forget any user_id
+#     session.clear()
+
+#     # Redirect user to login form
+#     return redirect("/")
+
+
+# @app.route("/quote", methods=["GET", "POST"])
+# @login_required
+# def quote():
+#     """Get stock quote."""
+#     return apology("TODO")
+
+
+# @app.route("/register", methods=["GET", "POST"])
+# def register():
+#     """Register user"""
+#     return apology("TODO")
+
+
+# @app.route("/sell", methods=["GET", "POST"])
+# @login_required
+# def sell():
+#     """Sell shares of stock"""
+#     return apology("TODO")
+
+
+# def errorhandler(e):
+#     """Handle error"""
+#     if not isinstance(e, HTTPException):
+#         e = InternalServerError()
+#     return apology(e.name, e.code)
+
+
+# # Listen for errors
+# for code in default_exceptions:
+#     app.errorhandler(code)(errorhandler)
+
 import os
 
 from cs50 import SQL
@@ -46,15 +189,17 @@ if not os.environ.get("API_KEY"):
 @login_required
 def index():
     """Show portfolio of stocks"""
+    # return apology("TODO")
     user_id = session["user_id"]
-    stocks = db.execute("SELECT symbol, name, price, SUM(shares), FROM transactions WHERE user_id = ? CROUP BY symbol", user_id)
+    stocks = db.execute(
+        "SELECT symbol, name, price, SUM(shares) as totalShares FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
     cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
 
     total = cash
 
     for stock in stocks:
         total += stock["price"] * stock["totalShares"]
-    return render_template("index.html", stocks = stocks, cash = cash, usd = usd)
+    return render_template("index.html", stocks=stocks, cash=cash, usd=usd, total=total)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -73,7 +218,7 @@ def buy():
         try:
             shares = int(request.form.get("shares"))
         except:
-            return apology('Shares must be an integer!')
+            return apology("Shares must be an integer!")
 
         if shares <= 0:
             return apology("Share must be a positive integer!")
@@ -90,21 +235,21 @@ def buy():
         else:
             db.execute("UPDATE users SET cash = ? WHERE id = ?", cash - total_price, user_id)
             db.execute("INSERT INTO transactions (user_id, name, shares, price, type, symbol) VALUES (?, ?, ?, ?, ?, ?)",
-                        user_name, item_name, shares, item_price, 'buy', symbol)
+                       user_id, item_name, shares, item_price, 'buy', symbol)
         print(f'\n\n{cash}\n\n')
 
-        return redirect('/')
+        return redirect("/")
     else:
         return render_template("buy.html")
+
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
-    transactions = db.execute("SELECT symbol, shares, price, transcted FROM transactions WHERE user_id=:user_id", user_id = session["user_id"])
-    for i in range(len(transactions)):
-        transactions[i]["price"] = usd(transactions[i]["price"])
-    return render_template("history.html", transactions = transaction)
+    user_id = session["user_id"]
+    transactions = db.execute("SELECT type, symbol, price, shares, time FROM transactions WHERE user_id = ?", user_id)
+    return render_template("history.html", transactions=transactions, usd=usd)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -160,7 +305,6 @@ def quote():
     """Get stock quote."""
     if request.method == "POST":
         symbol = request.form.get("symbol")
-
         if not symbol:
             return apology("Please enter a symbol!")
 
@@ -169,7 +313,7 @@ def quote():
         if not item:
             return apology('Invalid symbol!')
 
-        return render_template("qupted.html", item = item, usd_function = usd)
+        return render_template("quoted.html", item=item, usd_function=usd)
     else:
         return render_template("quote.html")
 
@@ -179,26 +323,30 @@ def register():
     """Register user"""
     if (request.method == "POST"):
         username = request.form.get('username')
-        passward = request.form.get('passward')
+        password = request.form.get('password')
         confirmation = request.form.get('confirmation')
 
+        # print(username)
+        # print(password)
+        # print(confirmation)
         if not username:
             return apology('Username is required!')
-        elif not passward:
-            return apology('Passward is required!')
+        elif not password:
+            return apology('Password is required!')
         elif not confirmation:
-            return apology('Passward confirmation is required!')
+            return apology('Password confirmation is required!')
 
-        if passward != confirmation:
-            return apology('Passwards do not match!')
+        if password != confirmation:
+            return apology('Passwords do not match!')
 
-        hash = generate_passward_hash(passward)
+        hash = generate_password_hash(password)
 
         try:
-            db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
-            return redirect('/')
+            db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, hash)
+            return redirect("/")
         except:
-            return apology('Username has already been registerd!')
+            return apology("You have already registered!")
+
     else:
         return render_template("register.html")
 
@@ -217,8 +365,9 @@ def sell():
 
         item_price = lookup(symbol)["price"]
         item_name = lookup(symbol)["name"]
+        price = shares * item_price
 
-        shares_owned = db.execute("SELECT shares FROM transaction WHERE user_id = ? AND symbol = ?", user_id, sumbol)
+        shares_owned = db.execute("SELECT shares FROM transactions WHERE user_id = ? AND symbol = ?", user_id, symbol)[0]["shares"]
 
         if shares_owned < shares:
             return apology("You don't have enough shares!")
@@ -226,13 +375,15 @@ def sell():
         current_cash = db.execute("SELECT cash FROM users WHERE id = ?", user_id)[0]["cash"]
         db.execute("UPDATE users SET cash = ? WHERE id = ?", current_cash + price, user_id)
         db.execute("INSERT INTO transactions (user_id, name, shares, price, type, symbol) VALUES (?, ?, ?, ?, ?, ?)",
-                    user_id, item_name, shares, item_price, "shell", symbol)
+                   user_id, item_name, -shares, item_price, "sell", symbol)
 
-        return redirect('/')
+        return redirect("/")
+
     else:
         user_id = session["user_id"]
-        symbols = db.execute("SELECT symbol FROM transaction WHERE user_id = ? GROUP BY symbol", user_id)
-        return render_template("shell.html", symbols = symbols)
+        symbols = db.execute("SELECT symbol FROM transactions WHERE user_id = ? GROUP BY symbol", user_id)
+        return render_template("sell.html", symbols=symbols)
+
 
 def errorhandler(e):
     """Handle error"""
